21eb27fd103f5db1d2aa1af54943cb8d4cb267ff
satp42 <msg4satwik@gmail.com>
2024-07-13 11:28:08 +0530

    Quartz sync: Jul 13, 2024, 11:28 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,9 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/satp42",
+      Twitter: "https://twitter.com/therealsatwik",
+      LinkedIn: "https://www.linkedin.com/in/satwik-panigrahi-90a7b1218/"
     },
   }),
 }
